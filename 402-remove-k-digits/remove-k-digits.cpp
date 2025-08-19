54fc184cca04174793d238618286d4d27c283a73
Yamini Sisodia <104514779+YAMMYY10@users.noreply.github.com>
2025-08-19 16:26:10 +0530

    Time: 5 ms (35.79%) | Memory: 11.8 MB (22.37%) - LeetSync

---
+++ 402-remove-k-digits/remove-k-digits.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+        stack<char> numStack;
+        
+            for (char digit : num) {
+            // While there are remaining removals (k) and the stack is not empty and the current digit is smaller than the top of the stack
+            while (k > 0 && !numStack.empty() && digit < numStack.top()) {
+                numStack.pop();
+                k--;
+            }
+            numStack.push(digit);
+        }
+        
+        // After iterating through the number, if there are still remaining removals, remove digits from the stack
+        while (k > 0 && !numStack.empty()) {
+            numStack.pop();
+            k--;
+        }
+
+        string temp = "";
+        while (!numStack.empty()) {
+            temp.push_back(numStack.top());
+            numStack.pop();
+        }
+        reverse(temp.begin(), temp.end());
+
+        int m = temp.size();
+        string result = "";
+        int foundNonZero = 0;
+        for (int i = 0; i < m; i++) {
+            if (temp[i] == '0' && foundNonZero == 0) {
+                continue;
+            } else {
+                result.push_back(temp[i]);
+                foundNonZero = 1;
+            }
+        }
+        if (result.size() == 0)
+            result.push_back('0');
+        return result;
+    }
+};
